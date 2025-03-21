023dfb2af019c48c579bc3d5fe3fadc9f3fe8213
issam-os <issam.bouyoussef94@gmail.com>
2025-03-21 12:14:32 +0000

    Update App.js

--- App.js
+++ App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './screens/Home';
 import Books from './screens/Books';
-import Tools from './screens/Tools';
+import ITTools from "./screens/ITTools";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/books" element={<Books />} />
-        <Route path="/tools" element={<Tools />} />
+        <Route path="/it-tools" element={<ITTools />} />
       </Routes>
     </Router>
   );
