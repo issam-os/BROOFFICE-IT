472dc4d2979f903ce8e3dd2ad49638b97f58aad0
issam-os <issam.bouyoussef94@gmail.com>
2025-04-13 22:24:18 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Books from './pages/Books'
 import Tools from './pages/Tools'
+import Header from './components/Header'
 import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/books" element={<Books />} />
