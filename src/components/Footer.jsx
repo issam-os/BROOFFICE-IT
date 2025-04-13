cd7f8391a2308680343ae4587295c62416f6aaea
issam-os <issam.bouyoussef94@gmail.com>
2025-04-13 22:15:46 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white py-6 text-center mt-10">
+      <p className="text-sm">&copy; {new Date().getFullYear()} BROOFFICE IT. Tous droits réservés.</p>
+    </footer>
+  )
+}
+
+export default Footer
